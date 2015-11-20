310d50840c646970700645d407f028885ea9bbca
avermeulen <andre.vermeulen@gmail.com>
2015-11-21 00:33:32 +0200

    added local storage support using pouch db

--- app.js
+++ app.js
@@ -15,28 +15,44 @@ function byId(id){
 
     ready(function(){
 
-        var subjectName = byId('subjectName');
+        var locationName = byId('locationName');
         var addSubjectButton = byId('addSubjectButton');
         var addSubjectList = byId('subjectList');
         var captureLocation = byId('captureLocation');
         var locations = byId('locations');
         var content = byId('content');
 
-        var subjectLength = 0;
+        var locationDatabase = new PouchDB('locations');
+        var currentKey = "";
+        var currentLocation = "";
 
-        /*
-        subjectName.addEventListener('input', function(){
-            subjectLength = subjectName.value.length;
-            if (subjectLength > 3){
-                //console.log(subjectName.value);
+        locationDatabase.allDocs({}, function(err, docs){
+            if (err) return alert(err);
+
+            var rows = docs.rows;
+            for (var itemIndex in rows){
+                var item = rows[itemIndex];
+                addSubjectList.innerHTML += "<li>" + item.key + "</li>";
             }
+
         });
-        */
 
         addSubjectButton.addEventListener('click', function(evt){
-            if (subjectName.value.length >= 3){
-                addSubjectList.innerHTML += "<li>" + subjectName.value + "</li>";
-                subjectName.value = "";
+            if (locationName.value.length >= 3){
+
+                var location = {
+                    _id : locationName.value,
+                    locations : []
+                };
+
+                locationDatabase.put(location, function(err, result){
+
+                    if (err) return alert(err);
+
+                    addSubjectList.innerHTML += "<li>" + locationName.value + "</li>";
+                    locationName.value = "";
+                });
+
             }
 
         });
@@ -44,12 +60,37 @@ function byId(id){
         addSubjectList.addEventListener('click', function(){
             //alert(event.target.innerHTML);
             content.className = "";
+            //
+            currentKey = event.target.innerHTML;
+            locationDatabase
+                .get(currentKey)
+                .then(function(theLocation){
+                    currentLocation = theLocation;
+                    locations.innerHTML = "";
+                    var locationList = currentLocation.locations;
+                    for (var i = 0; i < locationList.length; i++) {
+                        var loc = locationList[i];
+                        var locationString = loc.lat + ", " + loc.long;
+                        locations.innerHTML += "<li>" + locationString + "</li>";
+                    }
+                });
+
         });
 
         captureLocation.addEventListener('click', function () {
             navigator.geolocation.getCurrentPosition(function(position) {
-                var lat_long = position.coords.latitude + ", " + position.coords.longitude;
-                locations.innerHTML += "<li>" + lat_long + "</li>";
+                var lat_long = { lat  : position.coords.latitude,
+                                 long : position.coords.longitude};
+
+                currentLocation.locations = currentLocation.locations || [];
+                currentLocation.locations.push(lat_long);
+
+                locationDatabase.put(currentLocation, function(err, location){
+
+                    var locationString = lat_long.lat + ", " + lat_long.long;
+                    locations.innerHTML += "<li>" + locationString + "</li>";
+
+                });
             });
         });
 
